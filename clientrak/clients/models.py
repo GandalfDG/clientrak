994344c4fa82e6c8ad03182f7714e202265fda2f
Jack Case <jackacase@gmail.com>
2022-10-22 23:09:06 +0000

    linked agents to the auth User model
    need to work on login/logout views

--- clientrak/clients/models.py
+++ clientrak/clients/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 import datetime
 
 class Agent(models.Model):
-    first_name = models.CharField(max_length=256)
-    last_name = models.CharField(max_length=256)
+    user = models.ForeignKey(User, models.CASCADE, null=True)
     minimum_rate = models.DecimalField(max_digits=8, decimal_places=2)
+
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
